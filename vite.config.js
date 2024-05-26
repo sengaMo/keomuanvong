d1685d5094d4f2ce4a4bb6034faa6b27d231b684
Mo <sengathit.info@gmail.com>
2024-05-26 20:01:34 +0700

    as
    as

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    outDir: "dist",
+    chunkSizeWarningLimit: 100000000,
+  },
+  preview: {
+    host: true,
+    port: 9090,
+  },
+});
